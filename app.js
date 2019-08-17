3aa82ede9caa5e8cf998f2df4e35909f59160d33
anber <shuangyan_m@hotmail.com>
2019-08-17 16:00:59 +0800

    support node 12

---
+++ app.js
@@ -0,0 +1,18 @@
+// Move the mouse across the screen as a sine wave.
+var robot = require("./index");
+
+// Type "Hello World" then press enter.
+// var robot = require("robotjs");
+
+// Type "Hello World".
+// robot.typeString("Hello World");
+
+// Press enter.
+robot.keyTap("shift");
+robot.keyTap("n");
+robot.keyTap("i");
+robot.keyTap("h");
+robot.keyTap("a");
+robot.keyTap("o");
+robot.keyTap("1");
+robot.keyTap("enter");
